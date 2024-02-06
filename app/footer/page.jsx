2440acd3cc424e4200b6966170c0201c8dea7ffd
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-02-06 15:24:27 +0600

    Footer added

---
+++ app/footer/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import FooterSection from "./Footer";
+
+const FooterPage = () => {
+  return (
+    <div>
+      <FooterSection />
+    </div>
+  );
+};
+
+export default FooterPage;
