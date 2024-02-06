bffb5240acfe5c7aee22b01dc711e184781415c7
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-02-06 20:13:25 +0600

    CreateUser and Dependencies added

---
+++ app/createUser/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateUserPage from "./CreateUserPage";
+
+const page = () => {
+  return (
+    <div>
+      <CreateUserPage />
+    </div>
+  );
+};
+
+export default page;
